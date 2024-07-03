da599a94732cbf9ec49c239791f201846a054758
ants-nav <ants.nav+github@gmail.com>
2024-07-03 20:31:28 +1000

    ahhhrhh dont know

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter , Depends, status, HTTPException, Response
+from fastapi import APIRouter , Depends, status, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from .. import database, schemas, models, utils, oauth2
+
+from .. import database, models, utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
